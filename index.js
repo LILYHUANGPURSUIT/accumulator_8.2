ca38b009d26646c332f4d68578fdc778fe8aabab
“Lili <lilyhuang114classb@gmail.com>
2021-07-22 17:05:46 -0400

    finished second exercise on 7/22/2021.

--- index.js
+++ index.js
@@ -37,11 +37,15 @@ const studentsArr = [
  * @returns {String} A string containing the names of all the students from the `students` array.
  */
 function stringNames(students) {
-
+  let allNames = "";
+  for (let i=0; i<students.length;i++) {
+    allNames += studentsArr[i].name + ", "
+  }
+  return allNames;
 }
 
-stringNames(studentsArr);
-//> "Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, John Dietrich, Dennis Schulist"
+console.log(stringNames(studentsArr));
+// > "Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, John Dietrich, Dennis Schulist"
 
 /**
  * Returns the names of each student in the class in an array.
@@ -49,75 +53,112 @@ stringNames(studentsArr);
  * @returns {String[]} An array of the names of all the students from the `students` array.
  */
 function arrayNames(students) {
-
+  let newArr = [];
+  for (let i=0; i<students.length;i++) {
+      newArr.push(studentsArr[i].name);
+  }
+  return newArr;
 }
+console.log(arrayNames(studentsArr));
+// > ['Leanne Graham', 'Ervin Howell', 'Clementine Bauch', 'Patricia Lebsack', 'John Dietrich', 'Dennis Schulist']
 
-arrayNames(studentsArr);
-//> ['Leanne Graham', 'Ervin Howell', 'Clementine Bauch', 'Patricia Lebsack', 'John Dietrich', 'Dennis Schulist']
-
-/**
- * Searches for a student by name.
- * @param {Object[]} students - An array of objects, each representing a student.
- * @param {String} name - The name of the student to find.
- * @returns {Object} The student in the class whose name matches.
- */
+// /**
+//  * Searches for a student by name.
+//  * @param {Object[]} students - An array of objects, each representing a student.
+//  * @param {String} name - The name of the student to find.
+//  * @returns {Object} The student in the class whose name matches.
+//  */
 function findByName(students, name) {
-
+  let findStudent = {};
+  for (let i=0; i<students.length;i++) {
+    let nameInArray = students[i].name;
+    if (nameInArray === name) {
+      findStudent = students[i];
+    }
+  }
+  return findStudent;
 }
 
-findByName(studentsArr, "Clementine Bauch");
-//> { "name": "Clementine Bauch", "GPA": 3.4, "role": "Teacher's Assistant" }
+console.log(findByName(studentsArr, "Clementine Bauch"));
+// > { "name": "Clementine Bauch", "GPA": 3.4, "role": "Teacher's Assistant" }
 
-findByName(studentsArr, "John Dietrich");
-//> { "name": "John Dietrich", "GPA": 3.7, "role": "Hall Monitor" }
+// findByName(studentsArr, "John Dietrich");
+// //> { "name": "John Dietrich", "GPA": 3.7, "role": "Hall Monitor" }
 
-/**
- * Returns the average (also known as the mean average) GPA for the class.
- * @param {Object[]} students - An array of objects, each representing a student.
- * @returns {Number} The average GPA for the class.
- */
+// /**
+//  * Returns the average (also known as the mean average) GPA for the class.
+//  * @param {Object[]} students - An array of objects, each representing a student.
+//  * @returns {Number} The average GPA for the class.
+//  */
 function findAverageGPA(students) {
-
+  let originalGpa = 0;
+  let totalNumber = students.length;
+  for (let i=0; i<students.length;i++) { 
+    originalGpa += students[i].GPA;
+    result = (originalGpa/totalNumber).toFixed(2);
+  }
+    return result;
 }
 
-findAverageGPA(studentsArr);
+console.log(findAverageGPA(studentsArr));
 //> 3.45
 
-/**
- * Returns the highest GPA for the class.
- * @param {Object[]} students - An array of objects, each representing a student.
- * @returns {Number} The highest GPA for the class.
- */
+// /**
+//  * Returns the highest GPA for the class.
+//  * @param {Object[]} students - An array of objects, each representing a student.
+//  * @returns {Number} The highest GPA for the class.
+//  */
 function findHighestGPA(students) {
-
+  let highestGpa = 0;
+  for (let i=0; i<students.length;i++) { 
+    let comparedGpa = students[i].GPA;
+    if (comparedGpa > highestGpa) {
+      highestGpa = comparedGpa;
+    }
+  }
+  return highestGpa.toFixed(1);
 }
 
-findHighestGPA(studentsArr);
-//> 4.0
+console.log(findHighestGPA(studentsArr));
+// //> 4.0
 
-/**
- * Returns the lowest GPA for the class.
- * @param {Object[]} students - An array of objects, each representing a student.
- * @returns {Number} The lowest GPA for the class.
- */
+// /**
+//  * Returns the lowest GPA for the class.
+//  * @param {Object[]} students - An array of objects, each representing a student.
+//  * @returns {Number} The lowest GPA for the class.
+//  */
 function findLowestGPA(students) {
-
+  let lowestGpa = students[0].GPA;
+  for (let i=1; i<students.length;i++) { 
+    let numberInArrGpa = students[i].GPA;
+    if (lowestGpa > numberInArrGpa) {
+      lowestGpa = numberInArrGpa;
+    }
+  }
+  return lowestGpa;
 }
 
-findLowestGPA(studentsArr);
+console.log(findLowestGPA(studentsArr));
 //> 2.9
 
-/**
- * Returns only the students who have a specific role.
- * @param {Object[]} students - An array of objects, each representing a student.
- * @param {String} role - The role of the desired students.
- * @returns {Object[]} The students who have the role.
- */
+// /**
+//  * Returns only the students who have a specific role.
+//  * @param {Object[]} students - An array of objects, each representing a student.
+//  * @param {String} role - The role of the desired students.
+//  * @returns {Object[]} The students who have the role.
+//  */
 function filterByRole(students, role) {
-
+  let outPutArr = [];
+  for (let i=0; i<students.length;i++) { 
+    let roleInArr = students[i].role;
+    if (roleInArr === role) {
+    outPutArr.push(students[i]);
+    }
+  }
+  return outPutArr;
 }
 
-filterByRole(studentsArr, "Hall Monitor");
+console.log(filterByRole(studentsArr, "Hall Monitor"));
 //> [
 //     {
 //         "name": "Ervin Howell",
@@ -131,16 +172,16 @@ filterByRole(studentsArr, "Hall Monitor");
 //     }
 // ]
 
-filterByRole(studentsArr, "Teacher's Assistant");
-//> [
-//     {
-//         "name": "Clementine Bauch",
-//         "GPA": 3.4,
-//         "role": "Teacher's Assistant",
-//     },
-//     {
-//         "name": "Dennis Schulist",
-//         "GPA": 3.0,
-//         "role": "Teacher's Assistant",
-//     }
-// ]
+// console.log(filterByRole(studentsArr, "Teacher's Assistant"));
+// //> [
+// //     {
+// //         "name": "Clementine Bauch",
+// //         "GPA": 3.4,
+// //         "role": "Teacher's Assistant",
+// //     },
+// //     {
+// //         "name": "Dennis Schulist",
+// //         "GPA": 3.0,
+// //         "role": "Teacher's Assistant",
+// //     }
+// // ]
